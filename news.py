ec873d6d53f926d9315727929285b563477c3e83
GedasGa <gedas.gardauskas@gmail.com>
2017-09-07 14:22:15 +0300

    Create two 2nd question  variables and implement

--- news.py
+++ news.py
@@ -6,12 +6,23 @@ DBNAME = "news"
 
 question_1 = "What are the most popular three articles of all time?"
 query_1 = """
-Select articles.title, count(log.status) as views
-from articles join log
-on log.path like concat('%', articles.slug)
-and log.status like '2%'
-group by articles.title
-order by views desc, articles.title limit 3;
+SELECT articles.title, COUNT(log.status) AS views
+FROM articles
+JOIN log ON log.path LIKE concat('%', articles.slug)
+AND log.status LIKE '2%'
+GROUP BY articles.title
+ORDER BY views DESC, articles.title LIMIT 3;
+"""
+
+question_2 = "What are the most popular three articles of all time?"
+query_2 = """
+SELECT authors.name, COUNT(log.status) AS views
+FROM authors
+JOIN articles ON articles.author = authors.id
+JOIN log ON log.path LIKE concat('%', articles.slug)
+AND log.status LIKE '2%'
+GROUP BY authors.name
+ORDER BY views DESC, authors.name;
 """
 
 def get_top_articles(question, query):
@@ -21,15 +32,24 @@ def get_top_articles(question, query):
   c.execute(query)
   results = c.fetchall()
   db.close()
-  print(question)
+  print(question) + '\n'
+  for i in results:
+      print "\"%s\" -- %s views" % (i[0], i[1])
+
+def get_popular_authors(question, query):
+  """Find the most popular article authors of all time"""
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute(query)
+  results = c.fetchall()
+  db.close()
+  print(question) + '\n'
   for i in results:
       print "%s -- %s views" % (i[0], i[1])
-
-def get_popular_authors():
-  """Find the most popular article authors of all time"""
 
 def get_error_percentage():
   """Find on which days did more than 1% of requests lead to errors"""
 
 if __name__ == '__main__':
     get_top_articles(question_1, query_1)
+    get_popular_authors(question_2, query_2)
