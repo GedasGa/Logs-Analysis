e3d170f47b7053a1af9b3e7a56a6ad4180e95ddf
GedasGa <gedas.gardauskas@gmail.com>
2017-09-07 13:59:29 +0300

    Add question and title

--- news.py
+++ news.py
@@ -4,6 +4,16 @@ import psycopg2
 
 DBNAME = "news"
 
+question_1 = "What are the most popular three articles of all time?"
+query_1 = """
+Select articles.title, count(log.status) as views
+from articles join log
+on log.path like concat('%', articles.slug)
+and log.status like '2%'
+group by articles.title
+order by views desc, articles.title limit 3;
+"""
+
 def get_top_articles(query):
   """Find the most popular three articles of all time"""
   db = psycopg2.connect(database=DBNAME)
@@ -11,9 +21,8 @@ def get_top_articles(query):
   c.execute(query)
   result = c.fetchall()
   db.close()
-
   for i in results:
-      print "%s -- %s views" % (i[0], i[1])  
+      print "%s -- %s views" % (i[0], i[1])
 
 def get_popular_authors():
   """Find the most popular article authors of all time"""
