e32c6d0328d3353bdf07ff7b07296b1d28ec808c
GedasGa <gedas.gardauskas@gmail.com>
2017-09-07 13:03:02 +0300

    Create 3 functions with comments

--- news.py
+++ news.py
@@ -3,3 +3,12 @@
 import psycopg2
 
 DBNAME = "news"
+
+def get_top_articles():
+  """Find the most popular three articles of all time"""
+
+def get_popular_authors():
+  """Find the most popular article authors of all time"""
+
+def get_error_percentage():
+  """Find on which days did more than 1% of requests lead to errors"""
